d146036b3735178f13c985040123102322e2b414
Parag Singh <parag.singh@geeksforgeeks.org>
2022-12-25 20:17:32 +0530

    Commiting

--- assets/js/main.js
+++ assets/js/main.js
@@ -253,36 +253,144 @@
   });
 
   let servies_data = [
-    "Workplace ergonomics",
-    "Fire safety",
-    "Workplace violence prevention",
-    "Employee health resources",
-    "Environmental safety",
-    "Equipment safety",
-    "Online safety and cyber security",
-    "Electrical safety",
-    "Hazard Communication Standards",
-    "Machinery and Equipment Safety Machine guarding",
-    "Chemical hazard substance",
-    "Scaffolding",
-    "Chemical safety",
-    "Construction site safety",
-    "Radiation safety",
-    "Driver Safety",
-    "Hazardous Waste Operations and Emergency Response (HAZWOPER)",
-    "Hazard Communication",
-    "Mining Safety",
-    "Incident Investigation and Reporting",
+    {
+      title: "Workplace ergonomics",
+      url: "assets/img/ergonomics.png",
+      description:
+        "Ergonomics is designing a job to fit the worker so the work is safer and more efficient. Implementing ergonomic solutions can make employees more comfortable and increase productivity.",
+    },
+    {
+      title: "Fire safety",
+      url: "assets/img/Fire-safety-at-public-places.jpg",
+      description:
+        "Active fire protection is an integral part of fire protection. AFP is characterized by items and/or systems, which require a certain amount of motion and response in order to work, contrary to passive fire protection.",
+    },
+    {
+      title: "Workplace violence prevention",
+      url: "assets/img/istockphoto-1068535750-612x612.jpg",
+      description:
+        "To reduce the possibility of workplace violence, companies must identify risk factors in their workplace and the way their workers operate, take necessary steps to limit those risks,& should build an extraordinary team culture. And we are not just talking about dangerous instances like shooting.",
+    },
+    {
+      title: "Employee health resources",
+      url: "assets/img/Texas_Health_Resources_Companies_Care.jpg",
+      description:
+        "Employee health encompasses the physical and mental status of your employees. It can cover illness and wellness in the same breath. When we talk about health, physical health comes into mind first, but mental health is just as important. And bad physical health can lead to bad mental health and vice versa.",
+    },
+    {
+      title: "Environmental safety",
+      url: "assets/img/Safety and Environmental Management.png",
+      description:
+        "Environment, health and safety, EHS is an acronym for the set that studies and implements the practical aspects of protecting the environment and maintaining health and safety at occupation. In simple terms it is what organizations must do to make sure that their activities do not cause harm to anyone.",
+    },
+    {
+      title: "Equipment safety",
+      url: "assets/img/equipment.jpg",
+      description:
+        "Safety equipment, also known as personal protective equipment, is specifically designed to help protect workers against health or safety risks on the jobsite. By wearing the appropriate safety equipment, workers can decrease the number of preventable accidents that occur on jobsites every year.",
+    },
+    {
+      title: "Online safety and cyber security",
+      url: "assets/img/online_safety.webp",
+      description:
+        "While cybersecurity protects devices and networks from harm by third parties, Online Safety protects the people using them from harm by the devices and networks (and therefore third parties) through awareness, education, information and technology.",
+    },
+    {
+      title: "Electrical safety",
+      url: "assets/img/workplace-electrical-safety.jpg",
+      description:
+        "Electrical safety is a system of organizational measures and technical means to prevent harmful and dangerous effects on workers from electric current, arcing, electromagnetic fields and static electricity.",
+    },
+    {
+      title: "Hazard Communication Standards",
+      url: "assets/img/hazard.jpg",
+      description:
+        "The Hazard Communication Standard requires employers in the United States to disclose toxic and hazardous substances in workplaces. This is related to the Worker Protection Standard.",
+    },
+    {
+      title: "Machinery and Equipment Safety Machine guarding",
+      url: "assets/img/machine_guards.jpg",
+      description:
+        "Machine guarding is a safety feature on or around manufacturing or other engineering equipment consisting of a shield or device covering hazardous areas of a machine to prevent contact with body parts or to control hazards like chips or sparks from exiting the machine.",
+    },
+    {
+      title: "Chemical hazard substance",
+      url: "assets/img/istockphoto-157529625-612x612.jpg",
+      description:
+        "A chemical hazard is a substance that has the potential to cause harm to life or health. Chemicals are widely used in the home and in many other places. Exposure to chemicals can cause acute or long-term detrimental health effects.",
+    },
+    {
+      title: "Scaffolding",
+      url: "assets/img/scaffolding-components.jpg",
+      description:
+        "Scaffolding, also called scaffold or staging, is a temporary structure used to support a work crew and materials to aid in the construction, maintenance and repair of buildings, bridges and all other man-made structures.",
+    },
+    {
+      title: "Chemical safety",
+      url: "assets/img/Origami-Risk.jpg",
+      description:
+        "Chemicals as elements, compounds, mixtures, solutions and emulsions are very widely used and transported in the modern industrial society. ",
+    },
+    {
+      title: "Construction site safety",
+      url: "assets/img/Construction.jpeg",
+      description:
+        "Construction site safety is an aspect of construction-related activities concerned with protecting construction site workers and others from death, injury, disease or other health-related risks.",
+    },
+    {
+      title: "Radiation safety",
+      url: "assets/img/istockphoto-692567132-612x612.jpg",
+      description:
+        "The guiding principle of radiation safety is “ALARA”. ALARA stands for “as low as reasonably achievable”. This principle means that even if it is a small dose, if receiving that dose has no direct benefit, you should try to avoid it.",
+    },
+    {
+      title: "Driver Safety",
+      url: "assets/img/driver.jpg",
+      description:
+        "As people age, changes occur in their mental and physical condition that may affect their ability to assess traffic situations and respond to them rapidly and appropriately. Having driven for many years, older individuals may not recognize these changes or the increased risk of accident associated with them. Driver safety assesses the ability of a driver, regardless of age, to control a vehicle and respond appropriately to changing traffic conditions.",
+    },
+    {
+      title: "Hazardous Waste Operations and Emergency Response (HAZWOPER)",
+      url: "assets/img/03102022_HAZWOPER_Banner.jpg",
+      description:
+        "HAZWOPER is the one of the most prominent international standards that governs health and safety work environments related to hazardous elements. Coined by OSHA, HAZWOPERM has been formulated as a regulatory standard which intends to protect employees working at sites of unprotected and uncontrolled hazardous wastes, performing clean-up operations of hazardous wastes without protective gears, engaged in waste treatment, storage and disposal plants and involved in emergency response activities related to hazardous items.",
+    },
+    {
+      title: "Hazard Communication",
+      url: "assets/img/images.jpeg",
+      description:
+        "The Hazard Communication Standard requires employers in the United States to disclose toxic and hazardous substances in workplaces. This is related to the Worker Protection Standard.",
+    },
+    {
+      title: "Mining Safety",
+      url: "assets/img/Safety-in-Mining.jpg",
+      description:
+        "Mine safety is a broad term referring to the practice of controlling and managing a wide range of hazards associated with the life cycle of mining-related activities.",
+    },
+    {
+      title: "Incident Investigation and Reporting",
+      url: "assets/img/Accident-Reporting-Investigation-Construction-Site.jpg",
+      description:
+        "Incident investigation is a process for reporting, tracking, and investigating incidents that includes a formal process for investigating incidents, including staffing, performing, documenting, and tracking investigations of process safety incidents and the trending of incident and incident investigation data to identify recurring incidents. ",
+    },
   ];
 
   const services_container = document.querySelector("#services_container");
   if (services_container) {
     servies_data.forEach((e) => {
       let div = document.createElement("div");
-      div.className = "col-md-6 col-lg-3";
-      div.setAttribute("data-aos", "zoom-in");
-      div.setAttribute("data-aos-delay", "100");
-      div.innerHTML = `<div class='icon-box'><h4 class='title'>${e}</h4></div>`;
+      div.className = "col-lg-4 col-md-6 portfolio-item filter-app";
+      div.innerHTML = `<div class="portfolio-wrap">
+    <img src="${e.url}" class="img-fluid" alt="" />
+    <div class="portfolio-links">
+      <a href="details.html" title="More Details">
+        <i class="bi bi-link"></i>
+      </a>
+    </div>
+    <div class="portfolio-info">
+      <h4>${e.title}</h4>
+    </div>
+  </div>`;
       services_container.appendChild(div);
     });
   }
